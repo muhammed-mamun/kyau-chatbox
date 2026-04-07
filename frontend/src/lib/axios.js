bd7d66589ec6dacf0c8848165dacaa3aa63a6720
Mamun Hossain <muhammed.mamun.131@gmail.com>
2026-04-07 23:47:47 +0600

    backend url added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://kyau-chatbox-git-main-md-mamun-hossains-projects-df3cc2b3.vercel.app/",
     withCredentials: true,
 });
